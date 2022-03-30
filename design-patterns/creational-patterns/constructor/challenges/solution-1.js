e388235ba777083da8a4515a18c175fc370071da
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-03-30 13:43:31 +0500

    constructor design pattern:challenge solved using es6 and base function prototype

---
+++ design-patterns/creational-patterns/constructor/challenges/solution-1.js
@@ -0,0 +1,9 @@
+function Human(name,age,occupation) {
+    this.name = name;
+    this.age = age;
+    this.occupation = occupation;
+}
+
+const jack = new Human('Jack' , 15 , 'Somewhere');
+
+console.log(jack);
---
+++ design-patterns/creational-patterns/constructor/challenges/solution.js
@@ -0,0 +1,9 @@
+class Human {
+    constructor(name,age,occupation) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+}
+
+const mike = new Human('mike' , 12 , 'Somewhere');
